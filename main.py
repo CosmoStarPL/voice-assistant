ba90b392162110a4473c2a22bd67dde5ec74254e
CosmoStarPL <cosmostarpl@gmail.com>
2024-03-05 02:51:59 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import sounddevice as sd
+from vosk import Model, KaldiRecognizer, SetLogLevel
+import numpy as np
+import json
+import subprocess
+
+SetLogLevel(-1)
+
+duration = 5
+fs = 16000
+channels = 1
+
+model = Model("model_small")
+rec = KaldiRecognizer(model, fs)
+
+while True:
+    recording = sd.rec(int(duration * fs), samplerate=fs, channels=channels, dtype='int16')
+    sd.wait()
+    recording_np = np.array(recording)
+
+    rec.AcceptWaveform(recording_np.tobytes())
+    
+    result = rec.PartialResult()
+    if "здравствуйте" in result:
+        pass
+
+    if result:
+        print(json.loads(result))
+    
+    if rec.FinalResult():
+        print(json.loads(rec.FinalResult()))
