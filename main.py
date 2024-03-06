cf015e0cc3216a41353e1fb6f5778a716059a958
Ulu-pro <web.ulu7@gmail.com>
2024-03-06 19:56:23 +0500

    Behavior when online and offline

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import requests
 import sounddevice as sd
 from vosk import Model, KaldiRecognizer, SetLogLevel
 import numpy as np
+
 import json
+
+import utility_text
+import utility_audio
+import utility_video
 
 SetLogLevel(-1)
 
@@ -10,21 +16,47 @@ fs = 16000
 channels = 1
 
 model = Model("model_small")
-rec = KaldiRecognizer(model, fs)
+recognizer = KaldiRecognizer(model, fs)
+
+default_phrase = "здравствуйте"
+default_reply = "ну здравствуй"
+
+url = "https://webulu.uz/voice/source.json"
+content = {}
+
+
+def main_online():
+    if content["prefer"] == 0:
+        utility_text.say(content["sources"]["text"])
+    elif content["prefer"] == 1:
+        utility_audio.play(content["sources"]["audio"])
+    elif content["prefer"] == 2:
+        utility_video.play(content["sources"]["video"])
+
+
+def main_offline():
+    utility_text.say(default_reply, False)
+
+
+def check_internet():
+    global content
+    try:
+        response = requests.get(url)
+        content = json.loads(response.content)
+        return True
+    except requests.RequestException:
+        return False
+
 
 while True:
     recording = sd.rec(int(duration * fs), samplerate=fs, channels=channels, dtype='int16')
     sd.wait()
     recording_np = np.array(recording)
 
-    rec.AcceptWaveform(recording_np.tobytes())
-    
-    result = rec.PartialResult()
-    if "здравствуйте" in result:
-        pass
+    recognizer.AcceptWaveform(recording_np.tobytes())
+    result = recognizer.FinalResult()
 
-    if result:
-        print(json.loads(result))
-    
-    if rec.FinalResult():
-        print(json.loads(rec.FinalResult()))
+    if check_internet() and content["powered"] and content["phrase"] in result:
+        main_online()
+    elif default_phrase in result:
+        main_offline()
