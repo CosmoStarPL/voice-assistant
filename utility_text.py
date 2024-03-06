28e1bc948f4ad8f5760ddf7cf203fa4b6e2b9119
CosmoStarPL <cosmostarpl@gmail.com>
2024-03-06 22:53:44 +0500

    Utility text

--- utility_text.py
+++ utility_text.py
@@ -1,2 +1,18 @@
+from gtts import gTTS
+import soundfile as sf
+import sounddevice as sd
+
 def say(text, is_online=True):
-    pass
+    file_name = "sound.mp3"
+    language = 'ru'
+
+    if is_online:
+        tts = gTTS(text=text, lang=language, slow=False)
+        tts.save(file_name)
+    else:
+        data, sr = sf.read("default.mp3")
+        sd.play(data, sr)
+
+    data, sr = sf.read(file_name)
+    sd.play(data, sr)
+    sd.wait()
